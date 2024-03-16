c9352205f0a5b0e8f19cdf52931f7af1e5f29b01
pinkjo8825 <83690209+pinkjo8825@users.noreply.github.com>
2024-03-16 10:42:37 +0700

    forget

--- src/main/java/com/net/runningwebservice/endpoint.java
+++ src/main/java/com/net/runningwebservice/endpoint.java
@@ -408,8 +408,6 @@ public class endpoint {
             queryStringBuilder.append("  ?event ro:isOrganizedBy ?org . ?org ro:OrganizationName \"").append(organizationReg).append("\" .\n");
         }
 
-
-
         if (standardReg != null && !standardReg.isEmpty()) {
             queryStringBuilder.append("  ?event ro:StandardOfEvent \"").append(standardReg).append("\" .\n");
         }
@@ -417,7 +415,6 @@ public class endpoint {
         if (levelReg != null && !levelReg.isEmpty()) {
             queryStringBuilder.append("  ?event ro:LevelOfEvent \"").append(levelReg).append("\" .\n");
         }
-
 
         queryStringBuilder.append("}");
 
@@ -428,11 +425,13 @@ public class endpoint {
         Query query = QueryFactory.create(queryString);
         QueryExecution qexec = QueryExecutionFactory.create(query, dataOnto);
         ResultSet resultSet = qexec.execSelect();
+        System.out.println("resultSet " + resultSet);
 
         while (resultSet.hasNext()) {
             QuerySolution solution = resultSet.nextSolution();
-
+            System.out.println("solution " + solution);
             String runningEventName = solution.contains("eventName") ? solution.getLiteral("eventName").getString() : "Unknown Event";
+            System.out.println("runningEventName " + runningEventName);
 
             GetEventResponse.RunningEvent event = new GetEventResponse.RunningEvent();
             event.setRunningEventName(runningEventName);
